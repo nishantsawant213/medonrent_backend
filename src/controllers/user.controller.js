73293cbc3ae7d2fd698e8e0cd7e4e3156c8f7229
10288678 <nishant.sawant@indiapost.gov.in>
2025-09-06 23:42:28 +0530

    removed seucre as not using https ssl

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -65,8 +65,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true,
-        
+        secure: false,
+        sameSite: "lax"
     };
 
     return res
